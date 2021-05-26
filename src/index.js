3bc91db833e3bdf572a09c28719edefccb6f26d0
Maxine <maxinemeurer@yahoo.com[remote origin]>
2021-05-26 11:33:20 -0400

    create campaign form

--- src/index.js
+++ src/index.js
@@ -3,35 +3,35 @@ const growFundGreenAdapter = new GrowFundGreenAdapter("http://127.0.0.1:3000")
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    addCreateForm()
+    Campaign.addCreateForm()
     growFundGreenAdapter.getCampaigns()
     listenEditDelete()
 })
 
-function addCreateForm(){
-    const formContainer = document.getElementById("form-container")
-    const form = document.createElement('form')
-    form.innerHTML = `<input id="name-input" placeholder='name' type='text'/><br><input id="campaign-submit" value='Create Campaign' type='submit'/>`
-        // <input id="description-input" placeholder='description' type='text'/><br>
-       // <input id="goal-input" placeholder='goal' type='text'/><br><input id="campaign-submit" value='Create Campaign' type='submit'/>`
-    // const input = document.createElement('input');
-    // input.setAttribute('type', 'text')
-    // input.type = "text"
-    formContainer.append(form)
+// function addCreateForm(){
+//     const formContainer = document.getElementById("form-container")
+//     const form = document.createElement('form')
+//     form.innerHTML = `<input id="name-input" placeholder='name' type='text'/><br><input id="campaign-submit" value='Create Campaign' type='submit'/>`
+//         // <input id="description-input" placeholder='description' type='text'/><br>
+//        // <input id="goal-input" placeholder='goal' type='text'/><br><input id="campaign-submit" value='Create Campaign' type='submit'/>`
+//     // const input = document.createElement('input');
+//     // input.setAttribute('type', 'text')
+//     // input.type = "text"
+//     formContainer.append(form)
 
-    form.addEventListener("submit", handleSubmit)
-}
+//     form.addEventListener("submit", handleSubmit)
+// }
 
-function handleSubmit(event) {
-    event.preventDefault()
-    const nameInput = event.target[0]
-    if(editMode) {
-        growFundGreenAdapter.editCampaign(editMode, nameInput)
-    }
-    else {
-        growFundGreenAdapter.createCampaign(nameInput)
-    }
-}
+// function handleSubmit(event) {
+//     event.preventDefault()
+//     const nameInput = event.target[0]
+//     if(editMode) {
+//         growFundGreenAdapter.editCampaign(editMode, nameInput)
+//     }
+//     else {
+//         growFundGreenAdapter.createCampaign(nameInput)
+//     }
+// }
 
 // could put in campaign class 
 function listenEditDelete() {
